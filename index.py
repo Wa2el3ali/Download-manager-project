2769246fbb5d542a70e320eb2d3073d5615bfc34
wa2el <wa2el.3ali@gmail.com>
2018-07-14 22:08:52 +0200

    Continue Coding

--- index.py
+++ index.py
@@ -1,6 +1,27 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
 import os
 from os import path
+import sys
 
+FORM_CLASS,_ = loadUiType(path.join(path.dirname(__file__), "main.ui"))
+
+
+class MainApp(QMainWindow, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(MainApp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
