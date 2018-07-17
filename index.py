11e32e11c614c83e0625602bc089a8134ad6168c
wa2el <wa2el.3ali@gmail.com>
2018-07-18 00:37:00 +0200

    PAFY library

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import os
 from os import path
 import sys
 import urllib.request
+import pafy
 
 FORM_CLASS,_ = loadUiType(path.join(path.dirname(__file__), "main.ui"))
 
@@ -25,6 +26,7 @@ class MainApp(QMainWindow, FORM_CLASS):
     def Handle_Buttons(self):
         self.pushButton.clicked.connect(self.Download)
         self.pushButton_2.clicked.connect(self.Handle_Browse)
+        self.pushButton_4.clicked.connect(self.Download_Youtube_video)
 
     def Handle_Browse(self):
         save_place = QFileDialog.getSaveFileName(self, caption='Save as', directory='.', filter='All Files (*.*)')
@@ -39,14 +41,29 @@ class MainApp(QMainWindow, FORM_CLASS):
             self.progressBar.setValue(percentage)
             QApplication.processEvents()
 
+    def Download_Youtube_video(self):
+        video_link = self.lineEdit_3.text()
+        v = pafy.new(video_link)
+        # print(v.title)
+        # print(v.duration)
+        # print(v.rating)
+        # print(v.author)
+        # print(v.length)
+        # print(v.keywords)
+        # print(v.thumb)
+        # print(v.videoid)
+        # print(v.viewcount)
+        print(v.allstreams)
+
     def Download(self):
         url = self.lineEdit.text()
         save_location = self.lineEdit_2.text()
 
+
         try:
             urllib.request.urlretrieve(url, save_location, self.Handle_Progress)
         except Exception:
-            QMessageBox.warning(self, 'Download ERROR', 'The Download  Failed')
+            QMessageBox.warning(self, 'Download ERROR', 'The Download Failed')
             return
 
         QMessageBox.information(self, 'Download Completed', 'The Download is Finished')
